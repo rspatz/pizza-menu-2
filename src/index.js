80a423616f42540ba67be2947a972e054888b77a
Robert <rspatz@live.com>
2024-10-23 20:44:15 -0500

    Change menu and add items

--- src/index.js
+++ src/index.js
@@ -71,11 +71,32 @@ function Menu() {
   return (
     <main className='menu'>
       <h2>Our Menu</h2>;
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name='Pizza Spinaci'
+        ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+        photoName='pizzas/spinaci.jpg'
+        price={10}
+      />
+      <Pizza
+        name='Pizza Funghi'
+        ingredients='Tomato, mozarella, mushrooms, red onions'
+        price={12}
+        photoName='pizzas/funghi.jpg'
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className='pizza'>
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
   );
 }
 
@@ -90,16 +111,6 @@ function Footer() {
     <footer className='footer'>
       {new Date().toLocaleTimeString()} We're currently Open
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src='pizzas/spinaci.jpg' alt='Pizza spinaci' />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
   );
 }
 
