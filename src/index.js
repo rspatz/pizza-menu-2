30cf96d0d4971d3ff1a6d7579b623a4197cfbeab
Robert <rspatz@live.com>
2024-10-29 08:02:10 -0500

    Adding fragments, improving code

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const pizzaData = [
     ingredients: 'Tomato, mozarella, and pepperoni',
     price: 15,
     photoName: 'pizzas/salamino.jpg',
-    soldOut: true
+    soldOut: false
   },
   {
     name: 'Pizza Prosciutto',
@@ -68,31 +68,43 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className='menu'>
-      <h2>Our Menu</h2>;
-      <ul className='pizzas'>
-        {pizzaData.map(pizza => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
+      <h2>Our menu</h2>
 
-        {/* You might want to memorize this:
-        {pizzaData.map(pizza => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))} */}
-      </ul>
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className='pizzas'>
+            {pizzas.map(pizza => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
+
       {/* <Pizza
-        name='Pizza Spinaci'
-        ingredients='Tomato, mozarella, spinach, and ricotta cheese'
-        photoName='pizzas/spinaci.jpg'
-        price={10}
-      />
-      <Pizza
-        name='Pizza Funghi'
-        ingredients='Tomato, mozarella, mushrooms, red onions'
-        price={12}
-        photoName='pizzas/funghi.jpg'
-      /> */}
+      name="Pizza Spinaci"
+      ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+      photoName="pizzas/spinaci.jpg"
+      price={10}
+    />
+    <Pizza
+      name="Pizza Funghi"
+      ingredients="Tomato, mushrooms"
+      price={12}
+      photoName="pizzas/funghi.jpg"
+    /> */}
     </main>
   );
 }
@@ -136,37 +148,45 @@ function Footer() {
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
-  // if (hour >= openHour && hour <= closeHour) alert("We're currently Open.");
-  // else alert("Sorry, We're closed.");
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry we're closed");
+
+  // if (!isOpen) return <p>CLOSED</p>;
+
   return (
     <footer className='footer'>
       {isOpen ? (
         <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
-          Sorry, we're closed. Feel free to visit us between {openHour} and
-          {closeHour}.
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
   );
+
+  // return React.createElement("footer", null, "We're currently open!");
 }
 
 function Order({ closeHour, openHour }) {
   return (
     <div className='order'>
       <p>
-        We're open from {openHour}:00 until {closeHour}:00. Come visit us or
-        order online!
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
       </p>
       <button className='btn'>Order</button>
     </div>
   );
 }
 
+// React v18
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// React before 18
+// ReactDOM.render(<App />, document.getElementById("root"));
